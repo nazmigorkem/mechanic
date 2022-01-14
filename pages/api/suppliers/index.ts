aed6b70f292e1231a86a53d8d34f00cd4f87943e
nazmigorkem <69164892+nazmigorkem@users.noreply.github.com>
2022-01-14 20:12:27 +0300

    added suppliers and removed unnecessary lines

--- pages/api/suppliers/index.ts
+++ pages/api/suppliers/index.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-	const resp = await fetch(`http://localhost:3000/parts`);
+	const resp = await fetch(`http://localhost:3000/suppliers`);
 	const result = await resp.json();
 	res.send(result);
 }
--- pages/suppliers.tsx
+++ pages/suppliers.tsx
@@ -1,3 +1,27 @@
+import { useState } from 'react';
+import useSWR from 'swr';
+import NavbarElement from '../components/Jobs/NavbarElement';
+import Table from '../components/Table';
+import { fetcher } from '../util/functions';
+
 export default function suppliers() {
-	return <div></div>;
+	const [selectedQuery, setSelectedQuery] = useState('suppliers');
+	const { data: suppliersData } = useSWR(`/api/suppliers`, fetcher);
+	if (!suppliersData) {
+		return <></>;
+	}
+	const { suppliers } = suppliersData;
+	return (
+		<div className='h-screen w-full flex flex-col items-center justify-center'>
+			<div className='flex flex-row self-start gap-4' style={{ marginLeft: '14%' }}>
+				<NavbarElement queryKey='suppliers' text='Suppliers' selectedQuery={selectedQuery} onClick={() => setSelectedQuery('suppliers')} />
+			</div>
+
+			<div className='h-3/4 w-3/4 rounded-md shadow-md bg-black bg-opacity-40 flex itmes-center justify-center p-24 flex-wrap overflow-auto'>
+				<table className='divide-y-4 divide-slate-600 border-2 border-gray-700 w-full text-center'>
+					{selectedQuery === 'suppliers' && <Table cols={['Supplier ID', 'City', 'Postal Code', 'Tax Number']} rows={suppliers} />}
+				</table>
+			</div>
+		</div>
+	);
 }
